9519f87f96271fb019a5b2fb163eb8bc89ec9b6d
byeol <jeongeunbyeol8@gmail.com>
2022-08-17 18:06:14 +0900

    add Counter text test

--- src/components/__test__/Counter.test.tsx
+++ src/components/__test__/Counter.test.tsx
@@ -1,9 +1,25 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import Counter from "../Counter";
 
 const CounterProps = {
   count: 0,
   onIncrease: jest.fn(),
   onDecrease: jest.fn(),
 };
+// CounterProps는 컴포넌트 Counter에 넣어줄 프롭스
+// jest.fn()는 가짜 함수
 
-describe("<Counter/>", () => {});
+describe("<Counter/>", () => {
+  test("스크린에 'Counter'라는 텍스트가 보이는지 확인하는 테스트", () => {
+    render(<Counter {...CounterProps} />);
+
+    const text = screen.getByText("Counter");
+    // screen.getByText()는 스크린에서 괄호안의 ""안 텍스트를 가지고 있는 HTMLEliment 찾아줌
+    expect(text).toBeInTheDocument();
+    // 위에서 text라는 변수에 넣어준 HTMLElimentrk Counter 컴포넌트 안에 있는지 테스트
+
+    // const text = screen.getByText(/Count/i);
+    // expect(text).toBeInTheDocument();
+    // // 정확한 Counter를 찾는게 아니라 "Count"를 포함하고 있는 텍스트 찾고 싶을 땐 이런 정규식 사용
+  });
+});
